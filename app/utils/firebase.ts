29cbad9a43e424e1cf71062a4945f50d6ce2d17b
TheGhossst <nandukrishnapkm97@gmail.com>
2024-12-27 21:53:24 +0530

    add: environment variables

--- app/utils/firebase.ts
+++ app/utils/firebase.ts
@@ -4,14 +4,14 @@ import { getAuth, signOut as firebaseSignOut, GoogleAuthProvider } from 'firebas
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDw1Ccfr34WB351RmVqf5dP8a_08HTjiyk",
-  authDomain: "birthday-app-2bcd2.firebaseapp.com",
-  databaseURL: "https://birthday-app-2bcd2-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "birthday-app-2bcd2",
-  storageBucket: "birthday-app-2bcd2.firebasestorage.app",
-  messagingSenderId: "783120276987",
-  appId: "1:783120276987:web:8b8eeaff035980e0423f5e",
-  measurementId: "G-5247WDS7H5",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase app
